66ae19e13663424d7a1b2e9daea4c722586f7ae9
Yaroslav Gozak <gozakyaroslav@gmail.com>
2026-07-05 20:25:22 +0300

    remove redundant source

--- run_control_drone.sh
+++ run_control_drone.sh
@@ -1,6 +1,5 @@
 docker exec -it uav_bootcamp_ardupilot_navigation-ardupilot-sitl-1 bash -lc \
  "source /opt/ros/humble/setup.bash && \
- source /root/ardu_ws/install/setup.bash && \
  python control_drone_gazebo.py \
  --guided_nogps \
  --no-wind \
